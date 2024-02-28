58732e8c36357fd1862d0b6f956635fa4a98ec4e
rpcdurai <dreamerdurai@gmail.com>
2024-02-28 16:04:43 -0700

    add another 1a file

---
+++ Assignment_1a_WebPage.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+</script>
+</head>
+<body>
+
+<h2>Dev Ops Assignment One - Change Conent</h2>
+
+<p id="demo">Body of the page</p>
+
+<button type="button" onclick="myFunction()">Click</button>
+
+</body>
+</html> 
